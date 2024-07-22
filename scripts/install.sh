d548cd2ea3df330c8012d765a2e26954f30f4d11
Michel Antonio <michel.antonio@lseg.com>
2024-07-23 01:54:41 +0200

    install arch bWt

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,21 +1,25 @@
 # Load common functions
 source ./scripts/common.sh
 
+echo "yoyoyo"
 
 # Install arch btw...
 function install_arch_btw() {
-
-  PACKAGE_FILE="${PACKAGE_DIR}/pacstrap_arch_btw.conf"
-  PACKAGES$(tr '\n' ''$PACKAGE_FILE)
-
-  genfstab -U -p /mnt >> /mnt/etc/fstab
-
-  pacstrap -K /mnt $PACKAGES
-
-  arch-chroot /mnt /bin/bash <<'EOF'
-  install_packages pacstrap_arch_btw.conf 
-  
+pacstrap -K /mnt base base-devel linux linux-firmware
+    genfstab -U -p /mnt >> /mnt/etc/fstab
+    arch-chroot /mnt /bin/bash <<EOF
+    pacman -Syu --noconfirm btrfs-progs neovim lvm2 grub efibootmgr dosfstools mtools os-prober networkmanager openssh sudo intel-ucode xorg xorg-server gnome gnome-tweaks gdm nvidia nvidia-utils
+    systemctl enable NetworkManager
+    systemctl enable sshd
+    systemctl enable gdm
+    ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+    echo "$LANGUAGE" > /etc/locale.gen
+    locale-gen
+    echo "LANG=$LANGUAGE" > /etc/locale.conf
+    echo "$HOSTNAME" > /etc/hostname
+    (echo "$PASSWORD"; echo "$PASSWORD") | passwd
+    useradd -m -G wheel -s $USER_SHELL $USERNAME
+    (echo "$PASSWORD"; echo "$PASSWORD") | passwd $USERNAME
+    echo "%wheel ALL=(ALL:ALL) ALL" >> /etc/sudoers
 EOF
-
-
 }
